80b4b374127ecb5f2b3685d0d98d9ca03c0df460
creatijourny <misbahulhaque.gmail.com>
2026-05-29 12:22:49 +0600

    conditional signin, signup applied

---
+++ src/app/login/page.jsx
@@ -0,0 +1,103 @@
+"use client";
+import { Card } from '@heroui/react';
+import React from 'react';
+import { Check } from "@gravity-ui/icons";
+import { Button, Description, FieldError, Form, Input, Label, TextField } from "@heroui/react";
+import { authClient } from '@/lib/auth-client';
+import { redirect } from 'next/navigation';
+import { toast } from 'react-toastify';
+
+
+
+const LoginPage = () => {
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        const user = Object.fromEntries(formData.entries());
+
+        const { data, error } = await authClient.signIn.email({
+            email: user.email,
+            password: user.password,            
+        });
+        console.log({data, error});
+
+        if (!error) {
+            // toast.success("Successfully registered!")
+            redirect('/');
+            
+        }
+
+        // if (error) {
+        //     toast.error("Email or password is incorrect", {
+        //         position: "top-right",
+        //         autoClose: 3000,
+        //     });
+        // };
+    }
+    return (
+        <div className='max-w-7xl mx-auto'>
+            <div className='text-center mb-4'>
+                <h1 className='text-2xl font-bold'>Please login</h1>
+                <p>Enjoy reading with Studynook</p>
+            </div>
+            <Card className='border rounded-md'>
+                <Form onSubmit={onSubmit} className="flex w-96 flex-col gap-4 space-y-3 p-3">
+                  
+                    <TextField
+                        isRequired
+                        name="email"
+                        type="email"
+                        validate={(value) => {
+                            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+                                return "Please enter a valid email address";
+                            }
+                            return null;
+                        }}
+                    >
+                        <Label>Email</Label>
+                        <Input placeholder="john@example.com" />
+                        <FieldError />
+                    </TextField>
+                    <TextField
+                        isRequired
+                        minLength={8}
+                        name="password"
+                        type="password"
+                        validate={(value) => {
+                            if (value.length < 8) {
+                                return "Password must be at least 8 characters";
+                            }
+                            if (!/[A-Z]/.test(value)) {
+                                return "Password must contain at least one uppercase letter";
+                            }
+                            if (!/[0-9]/.test(value)) {
+                                return "Password must contain at least one number";
+                            }
+                            return null;
+                        }}
+                    >
+                        <Label>Password</Label>
+                        <Input placeholder="Enter your password" />
+                        <Description>Must be at least 8 characters with 1 uppercase and 1 number</Description>
+                        <FieldError />
+                    </TextField>
+                    <div className="flex justify-center">
+                        <Button type="submit" className={'w-full rounded-none bg-cyan-500'}>
+
+                            Login
+                        </Button>
+                        
+
+                    </div>
+                </Form>
+
+
+            </Card>
+            
+        </div>
+    );
+};
+
+export default LoginPage;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,63 @@
+'use client'
+import { authClient } from '@/lib/auth-client';
+import { Avatar, Button } from '@heroui/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
+
+    const {
+        data: session,
+        // isPending, //loading state
+        // error, //error object
+        // refetch //refetch the session
+    } = authClient.useSession()
+
+    const user = session?.user
+    console.log(user);
+
     return (
         <nav className='flex justify-between items-center px-4 bg-white'>
             <div>
-                <Image 
-                src={"/assets/study_logo.png"}
-                height= {130}
-                width={130}
-                alt='Image'/>
+                <Image
+                    src={"/assets/study_logo.png"}
+                    height={130}
+                    width={130}
+                    alt='Image' />
             </div>
             <div>
                 <ul>
-                <li className='flex gap-2 md:gap-4'>
-                    <Link href={'/'}>Home</Link>
-                    <Link href={'/rooms'}>Rooms</Link>
-                    <Link href={'/add-room'}>Add Room</Link>
-                    <Link href={'/my-listings'}>My Listings</Link>
-                    <Link href={'/my-bookings'}>My Bookings</Link>
-                    
-                    <Link href={'/profile'}>Profile</Link>
-                </li>
-            </ul>
+                    <li className='flex gap-2 md:gap-4'>
+                        <Link href={'/'}>Home</Link>
+                        <Link href={'/rooms'}>Rooms</Link>
+                        <Link href={'/add-room'}>Add Room</Link>
+                        <Link href={'/my-listings'}>My Listings</Link>
+                        <Link href={'/my-bookings'}>My Bookings</Link>
+
+                        <Link href={'/profile'}>Profile</Link>
+                    </li>
+                </ul>
             </div>
-            
+
             <div>
-                <ul className='flex gap-2'>                
-                <li><Link href={'/login'}>Login</Link></li>
-                <li><Link href={'/signup'}>Sign up</Link></li>
-            </ul>
+                <ul className='flex gap-2'>
+                    {user ? <>
+                        <li>
+                            <Avatar>
+                                <Avatar.Image alt="John Doe" src={user?.image} />
+                                <Avatar.Fallback>{user.name[0]}</Avatar.Fallback>
+                            </Avatar>
+                        </li>
+                        <li>
+                            <Button className={"rounded-none"}>Logout</Button>
+                        </li>
+                    </> : <>
+                        <li><Link href={'/login'}>Login</Link></li>
+                        <li><Link href={'/signup'}>Sign up</Link></li>
+                    </>}
+
+                </ul>
             </div>
         </nav>
     );
