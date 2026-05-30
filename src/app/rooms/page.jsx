919da524c82ffd70556d2ed0cb4f3e57f1e90078
creatijourny <misbahulhaque.gmail.com>
2026-05-31 03:15:08 +0600

    protect pages through proxy js applied

--- src/app/rooms/page.jsx
+++ src/app/rooms/page.jsx
@@ -8,7 +8,7 @@ const AllRoomsPage = async () => {
     console.log(rooms);
     return (
         <div className="container mx-auto p-4">
-            <h1 className="text-2xl font-bold mb-5">All Rooms</h1>
+            <h1 className="text-3xl font-bold mb-5">Study Rooms</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">            
             {
                 rooms.map(room => <RoomCard key={room._id} room={room}/>)
